b67ac8765be56297a83e6474373ed1144ab67fa0
Mario Kerndler <mini.korni@gmail.com>
2015-04-08 16:55:36 +0200

    Camera

---
+++ Scripts/MovingCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		while(true) {
+			StartCoroutine("Move");
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.localPosition += new Vector3(0,0.1f,0);
+	}
+
+	IEnumerator Move() {
+		print("Move");
+
+
+
+		yield return null;
+	}
+}

615802daac97a6b34269cd5ced7ac1002c59f4ab
David Holy <davidholy@me.com>
2015-04-09 14:20:26 +0200

    dsthdtg

---
+++ Scripts/bulletScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+
+
+	public Vector2 speed = new Vector2(6f, 0);
+	Rigidbody2D bulletBody;
+
+	void Start () {  
+
+		bulletBody = GetComponent <Rigidbody2D>();
+//		Vector2 bulletbodyVelocity = Rigidbody2D.velocity;
+//		bulletbodyVelocity.x = speed;
+		bulletBody.AddForce (speed, ForceMode2D);
+	}
+	
+
+	void OnBecameInvisible() {  
+
+		Destroy(gameObject);
+	}
+}

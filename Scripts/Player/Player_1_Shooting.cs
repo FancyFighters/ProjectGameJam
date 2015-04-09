2e6374e86a3c3b20341383e456e093cdb49756da
Emis987 <sob15669@spengergasse.at>
2015-04-09 12:45:24 +0100

    Changing Shooting #1

--- Scripts/Player/Player_1_Shooting.cs
+++ Scripts/Player/Player_1_Shooting.cs
@@ -9,7 +9,7 @@ public class Player_1_Shooting : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(Input.GetKeyDown(KeyCode.Minus))
 		{
 			Instantiate(bullte, player.transform.position,Quaternion.identity);
 		}

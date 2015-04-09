868f19295feb34c01b7f824102f88da4009e0c00
David Holy <davidholy@me.com>
2015-04-09 10:57:57 +0200

    level generation update

--- Scripts/gameLogic.cs
+++ Scripts/gameLogic.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 
 public class gameLogic : MonoBehaviour {
 
-	public Transform[] segments;
+	public GameObject[] segments;
 	private Vector3 nextSegment;
-	public List<Transform> spawnedSegments = new List<Transform>();
+	public List<GameObject> spawnedSegments = new List<GameObject>();
 	public float objectHeight = 5f;
 
 	// Use this for initialization
@@ -19,18 +19,24 @@ public class gameLogic : MonoBehaviour {
 	// Update is called once per frame
 	void spawnSegments () 
 	{
-		int randSegment = Random.Range (0, segments.Length);
-		Transform currSegment = Instantiate (segments [randSegment], nextSegment, Quaternion.identity) as Transform;
+		int randSegment = Random.Range (0, segments.Length-1);
+		//Debug.Log (segments[0]);
+		GameObject currSegment = Instantiate (segments [randSegment], nextSegment, Quaternion.identity) as GameObject;
 		spawnedSegments.Add (currSegment);
 
 		nextSegment += new Vector3 (0, objectHeight, 0);
 		print (nextSegment);
-		Invoke ("spawnSegments", 0.5f);
+		Invoke ("spawnSegments", 2f);
 
 	}
 
-	void destroySegments ()
+	void Update ()
 	{
-
+		if (spawnedSegments.Count > 3)
+		{
+			//print (spawnedSegments[0]);
+			Destroy(spawnedSegments[0]);
+			spawnedSegments.RemoveAt(0);
+		}
 	}
 }

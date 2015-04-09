e4fe4adf01f888ee605509bc0aa672c0ad8f09f3
Emis987 <sob15669@spengergasse.at>
2015-04-09 11:36:10 +0100

    Adding Shooting

---
+++ Scripts/Player/Player_2_Shooting.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_2_Shooting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

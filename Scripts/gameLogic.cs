8ed081de42de73211508184bd80b497a36a5bb56
David Holy <davidholy@me.com>
2015-04-09 16:48:47 +0200

    ENDLESS LEVEL

--- Scripts/gameLogic.cs
+++ Scripts/gameLogic.cs
@@ -16,37 +16,44 @@ public class gameLogic : MonoBehaviour {
 	public float objectHeight = 1f;
 	public float wallWidth = 1f;
 	public float segmentWidth = 1f;
-	public float spawningTime = 1f;
+	public float spawningTime = 6f;
+	private float timer = 0f;
 
 	void Start () 
 	{
+
 		nextLeftSegmentPosition = new Vector3 (0, 0, 0);
 		nextRightSegmentPosition = new Vector3 (segmentWidth + wallWidth, 0, 0);
-		nextMiddleWallSegmentPosition = new Vector3 (segmentWidth, 0, 0);
+		nextMiddleWallSegmentPosition = new Vector3 ((segmentWidth / 2)+ (wallWidth / 2), 0, 0);
 		spawnSegments ();
+		spawnSegments ();
+
+
 
 	}
 
 	void spawnSegments () 
 	{
-		int randLeftSegment = Random.Range (0, leftSegments.Length-1);
-		int randRightSegment = Random.Range (0, rightSegments.Length-1);
-		int randWallSegment = Random.Range (0, middleWallSegments.Length-1);
 
-		//left -------------------------------------------------------------------------------------------------------
-		GameObject currLeftSegment = Instantiate (leftSegments [randLeftSegment], nextLeftSegmentPosition, Quaternion.identity) as GameObject;
-		spawnedLeftSegments.Add (currLeftSegment);
-		nextLeftSegmentPosition += new Vector3 (0, objectHeight, 0);
-		//right ------------------------------------------------------------------------------------------------------
-		GameObject currRightSegment = Instantiate (rightSegments [randRightSegment], nextRightSegmentPosition, Quaternion.identity) as GameObject;
-		spawnedRightSegments.Add (currRightSegment);
-		nextRightSegmentPosition += new Vector3 (0, objectHeight, 0);
-		//wall -------------------------------------------------------------------------------------------------------
-		GameObject currWallSegment = Instantiate (middleWallSegments [randWallSegment], nextMiddleWallSegmentPosition, Quaternion.identity) as GameObject;
-		spawnedMiddleWallSegments.Add (currWallSegment);
-		nextMiddleWallSegmentPosition += new Vector3 (0, objectHeight, 0);
 
-		Invoke ("spawnSegments", spawningTime);
+
+			int randLeftSegment = Random.Range (0, leftSegments.Length - 1);
+			int randRightSegment = Random.Range (0, rightSegments.Length - 1);
+			int randWallSegment = Random.Range (0, middleWallSegments.Length - 1);
+
+			//left -------------------------------------------------------------------------------------------------------
+			GameObject currLeftSegment = Instantiate (leftSegments [randLeftSegment], nextLeftSegmentPosition, Quaternion.identity) as GameObject;
+			spawnedLeftSegments.Add (currLeftSegment);
+			nextLeftSegmentPosition += new Vector3 (0, objectHeight, 0);
+			//right ------------------------------------------------------------------------------------------------------
+			GameObject currRightSegment = Instantiate (rightSegments [randRightSegment], nextRightSegmentPosition, Quaternion.identity) as GameObject;
+			spawnedRightSegments.Add (currRightSegment);
+			nextRightSegmentPosition += new Vector3 (0, objectHeight, 0);
+			//wall -------------------------------------------------------------------------------------------------------
+			GameObject currWallSegment = Instantiate (middleWallSegments [randWallSegment], nextMiddleWallSegmentPosition, Quaternion.identity) as GameObject;
+			spawnedMiddleWallSegments.Add (currWallSegment);
+			nextMiddleWallSegmentPosition += new Vector3 (0, objectHeight, 0);
+			
 	}
 
 	void Update ()
@@ -55,6 +62,7 @@ public class gameLogic : MonoBehaviour {
 		{
 			Destroy(spawnedLeftSegments[0]);
 			spawnedLeftSegments.RemoveAt(0);
+
 
 		}
 
@@ -69,5 +77,15 @@ public class gameLogic : MonoBehaviour {
 			Destroy(spawnedMiddleWallSegments[0]);
 			spawnedMiddleWallSegments.RemoveAt(0);
 		}
+
+		timer += Time.deltaTime;
+
+		if (timer > spawningTime) 
+		{
+			timer -= spawningTime;
+			spawnSegments();
+
+		}
+
 	}
 }

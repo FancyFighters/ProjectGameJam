3a55275b01b724539fcb78e9ace1945057834b80
David Holy <davidholy@me.com>
2015-04-10 12:36:27 +0200

    axt changes, final collider changes

--- Scripts/Axt.cs
+++ Scripts/Axt.cs
@@ -2,24 +2,9 @@
 using System.Collections;
 
 public class Axt : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	void FixedUpdate () {
+		transform.localPosition += new Vector3(0,-0.05f,0);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnTriggerEnter2D (Collider2D other)
-	{
-		Debug.Log(other.gameObject.tag);
-		if (other.gameObject.tag == "Player") 
-		{
-			Destroy(gameObject);
-
-		}
-	}
 }
